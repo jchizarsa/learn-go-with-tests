150c426111d1c1533548c5b91f07ea8b6ac4febb
JC Hizarsa <75799709+jchizarsa@users.noreply.github.com>
2025-05-16 22:47:28 -0700

    Integers chapter

---
+++ integers/adder.go
@@ -0,0 +1,13 @@
+package integers
+
+/*
+ * Add() - Takes two input parameters, x and y, and returns their sum.
+ * @params
+ * int x
+ * int y
+ * @returns
+ * int - sum of x and y
+ */
+func Add(x, y int) int {
+	return x + y
+}
