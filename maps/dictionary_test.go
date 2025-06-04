459381fe287aeeaeaaef0201ba8f5a59f3a42e09
JC <75799709+jchizarsa@users.noreply.github.com>
2025-06-03 21:05:31 -0700

    maps part 1

---
+++ maps/dictionary_test.go
@@ -0,0 +1,20 @@
+package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is a test"}
+
+	got := dictionary.Search("test")
+	want := "this is a test"
+
+	AssertStrings(t, got, want)
+}
+
+func AssertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
