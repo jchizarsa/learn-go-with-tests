49a3300837b5ad8ea220803541095329027cbeab
JC Hizarsa <75799709+jchizarsa@users.noreply.github.com>
2025-06-06 00:59:13 -0700

    getting into dependency injection

---
+++ dependency-injection/di.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	// fmt.Printf("Hello, %s", name) // Defaults the Writer to stdout, don't want this
+	fmt.Fprintf(writer, "Hello, %s", name) // Allows us to pass in a writer, want this
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+func main() {
+	// Greet(os.Stdout, "Elodie") // Need to change type from *bytes.Buffer -> io.Writer
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
